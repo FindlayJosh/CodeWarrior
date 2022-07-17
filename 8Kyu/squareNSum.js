47d14a6f5fab35d5f4f4cfa32b51792e0e7e921c
FindlayJosh <joshuafindlay@gmail.com>
2022-07-17 14:00:00 +0100

    'square(n)Sum'

---
+++ 8Kyu/squareNSum.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/515e271a311df0350d00000f
+
+function squareSum(numbers){
+    let sqSum= 0 
+  for (let i =0; i < numbers.length; i++)
+     sqSum += numbers[i]**2
+    return sqSum
+  }
