a329978ddcfa3da7e7eb8c8ae4202fad28fd6bf2
FindlayJosh <joshuafindlay@gmail.com>
2022-07-28 14:00:00 +0100

    '7/28/22'

---
+++ 8Kyu/centuryFromYear.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+
+function century(year) {
+    if ( year % 100 === 0) {
+      return (year / 100);
+    } else {
+      return Math.ceil(year/100);
+    }
+  }
