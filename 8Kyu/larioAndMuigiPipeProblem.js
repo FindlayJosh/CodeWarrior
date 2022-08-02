9e2b04351d691a6b4294ca2002eb100d357d92eb
FindlayJosh <joshuafindlay@gmail.com>
2022-08-01 20:19:50 -0400

    8/1/22

---
+++ 8Kyu/larioAndMuigiPipeProblem.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56b29582461215098d00000f
+
+function pipeFix(numbers){
+    let array = []
+      for (let i = numbers[0]; i <= numbers[numbers.length -1]; i++) {
+        array.push(i)
+      }
+      return array
+    }
