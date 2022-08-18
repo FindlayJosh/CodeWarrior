fd7639de3555549a8bf89950920a42cf57673bb0
FindlayJosh <joshuafindlay@gmail.com>
2022-08-18 14:00:00 +0100

    'mmhmm'

---
+++ 8Kyu/breakingchocolateproblem.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/534ea96ebb17181947000ada
+
+function breakChocolate(n,m) {
+    if(n===0|| n===undefined||n===null|| m===0|| m===undefined|| m=== null){
+      return 0
+    }else 
+    return n*m-1
+  }
