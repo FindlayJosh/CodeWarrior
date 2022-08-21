4dc3656d47c6d8d46e0974dbee154441bc1e2bc2
FindlayJosh <joshuafindlay@gmail.com>
2022-08-21 14:00:00 +0100

    'mmhmm'

---
+++ 8Kyu/rockPaperScissorsLizardSpock.js
@@ -0,0 +1,53 @@
+https://www.codewars.com/kata/57d29ccda56edb4187000052
+
+function rpsls(pl1,pl2){
+    if (pl1 === "rock" && pl2 === "lizard"){
+      return 'Player 1 Won!'
+    }else if( pl1 === 'paper' && pl2 === 'rock'){
+      return 'Player 1 Won!'
+    } else if (pl1=== 'scissors' && pl2 === 'paper'){
+      return 'Player 1 Won!'
+    } else if (pl1 === 'lizard!' && pl2 === 'spock'){
+      return 'Player 1 Won!'
+    }else if(pl1 === 'spock' && pl2=== 'scissors'){
+      return 'Player 1 Won!'
+    }else if(pl1=== 'scissors' && pl2=== 'lizard'){
+      return 'Player 1 Won!'
+    }else if (pl1=== 'lizard' && pl2 === 'paper'){
+      return 'Player 1 Won!'
+    }else if(pl1=== 'paper' && pl2 === 'spock'){return 'Player 1 Won!'
+    
+      }else if (pl1 === 'spock' && pl2 === 'rock'){
+        return 'Player 1 Won!'
+      }else if(pl1 === 'rock' && pl2 ==='scissors'){
+        return 'Player 1 Won!'
+      } else if (pl2 === "rock" && pl1 === "lizard"){
+      return 'Player 2 Won!'
+    }else if( pl2 === 'paper' && pl1 === 'rock'){
+      return 'Player 2 Won!'
+    } else if (pl2=== 'scissors' && pl1 === 'paper'){
+      return 'Player 2 Won!'
+    } else if (pl2 === 'lizard!' && pl1 === 'spock'){
+      return 'Player 2 Won!'
+    }else if(pl2 === 'spock' && pl1=== 'scissors'){
+      return 'Player 2 Won!'
+    }else if(pl2=== 'scissors' && pl1=== 'lizard'){
+      return 'Player 2 Won!'
+    }else if (pl2=== 'lizard' && pl1 === 'paper'){
+      return 'Player 2 Won!'
+    }else if(pl2=== 'paper' && pl1 === 'spock'){
+      return 'Player 2 Won!'
+      }else if (pl2 === 'spock' && pl1 === 'rock'){
+        return 'Player 2 Won!'
+      }else if(pl2 === 'rock' && pl1 ==='scissors'){
+        return 'Player 2 Won!'
+    }else if(pl2 === 'spock' && pl1 ==='lizard'){
+      return 'Player 1 Won!'
+    }
+    else if (pl1 === pl2) {
+    return 'Draw!'
+    }
+    else if(pl1 ==='spock' && pl2 === 'lizard'){
+     return 'Player 2 Won!'
+    }
+      }
