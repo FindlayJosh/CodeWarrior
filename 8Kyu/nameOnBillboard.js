99130c25550bdbf307965bebd58c84ca01ec5c61
FindlayJosh <joshuafindlay@gmail.com>
2022-08-22 17:30:34 -0400

    8/22/22

--- 8Kyu/nameOnBillboard.js
+++ 8Kyu/nameOnBillboard.js
@@ -2,9 +2,7 @@
 
 function billboard(name, price = 30){
     let boop = name.split('')
-    let boopPop = boop.length
-      let cost =0
-    //   for (let i=0; i <= boopPop; i++)
+      let cost = 0
         boop.forEach(el => cost +=price)
       return cost
     } 
