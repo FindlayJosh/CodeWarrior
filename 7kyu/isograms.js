6cb684abc395035e7265fd009f74edd73ea15751
FindlayJosh <joshuafindlay@gmail.com>
2022-08-24 16:01:24 -0400

    8/24/22

---
+++ 7kyu/isograms.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/54ba84be607a92aa900000f1/javascript
+
+function isIsogram(str){
+    var i, j;
+    str = str.toLowerCase();
+    for(i = 0; i < str.length; i++)
+      for(j = i + 1; j < str.length; j++)
+        if(str[i] === str[j])
+          return false
+    return true
+  }
